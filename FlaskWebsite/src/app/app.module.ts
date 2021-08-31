e65716c26fba8323df430e24a601e9dda390874a
Bradley Doodnath <bradleydood999@gmail.com>
2021-08-30 21:57:32 -0400

    Clean website

--- FlaskWebsite/src/app/app.module.ts
+++ FlaskWebsite/src/app/app.module.ts
@@ -1,20 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PersonalInfoComponent } from './personal-info/personal-info.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    PersonalInfoComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, PersonalInfoComponent],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
