34df724b1d1c39dfa299f06f0ef9afeb54537c1e
Bradley Doodnath <bradleydood999@gmail.com>
2021-08-30 21:37:00 -0400

    Inital Commit

---
+++ FlaskWebsite/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PersonalInfoComponent } from './personal-info/personal-info.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PersonalInfoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
