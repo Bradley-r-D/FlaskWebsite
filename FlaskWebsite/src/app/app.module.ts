b98a1dbecd66e3a6aed6f463d0bca50ca2a315a5
Bradley Doodnath <bradleydood999@gmail.com>
2021-08-31 16:55:37 -0400

    Added color theme manipulation

--- FlaskWebsite/src/app/app.module.ts
+++ FlaskWebsite/src/app/app.module.ts
@@ -6,10 +6,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PersonalInfoComponent } from './personal-info/personal-info.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavpageComponent } from './navpage/navpage.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
-  declarations: [AppComponent, PersonalInfoComponent],
-  imports: [BrowserModule, FormsModule, AppRoutingModule, BrowserAnimationsModule],
+  declarations: [AppComponent, PersonalInfoComponent, NavpageComponent],
+  imports: [BrowserModule, FormsModule, AppRoutingModule, BrowserAnimationsModule, LayoutModule, MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule],
   providers: [],
   bootstrap: [AppComponent],
 })
