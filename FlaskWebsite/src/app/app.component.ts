719835e6cea350136c104f63000f488a654387e6
Bradley Doodnath <bradleydood999@gmail.com>
2021-08-31 16:55:11 -0400

    Added colour theme manipulation

--- FlaskWebsite/src/app/app.component.ts
+++ FlaskWebsite/src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'FlaskWebsite';
+  private isDark = false;
+
+  @HostBinding('class')
+  get themeMode() {
+    return this.isDark ? 'theme-dark' : 'theme-light';
+  }
 }
