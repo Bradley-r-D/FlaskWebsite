def41241fe7b2e197869ca704f26bb211e20e5ac
Bradley Doodnath <bradleydood999@gmail.com>
2021-09-27 13:48:04 -0400

    Altered Navbar for responsiveness

--- FlaskWebsite/src/app/app.module.ts
+++ FlaskWebsite/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @NgModule({
   declarations: [AppComponent, PersonalInfoComponent, NavpageComponent],
@@ -27,6 +28,7 @@ import { MatListModule } from '@angular/material/list';
     MatSidenavModule,
     MatIconModule,
     MatListModule,
+    MatTabsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- FlaskWebsite/src/app/components/navpage/navpage.component.ts
+++ FlaskWebsite/src/app/components/navpage/navpage.component.ts
@@ -1,8 +1,9 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, ViewChild } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
 import { ActivatedRoute } from '@angular/router';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-navpage',
@@ -18,10 +19,31 @@ export class NavpageComponent {
       shareReplay()
     );
 
+  @ViewChild(MatSidenav)
+  drawer!: MatSidenav;
+
   constructor(
     private breakpointObserver: BreakpointObserver,
     private route: ActivatedRoute
   ) {}
 
   page_name = 'Welcome to My Website';
+  showValues = true;
+  changeValues() {
+    console.log(this.showValues);
+    this.showValues = !this.showValues;
+  }
+  ngAfterViewInit(): void {
+    //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
+    //Add 'implements AfterViewInit' to the class.
+    this.breakpointObserver.observe(['(max-width: 800px']).subscribe((res) => {
+      if (res.matches) {
+        this.drawer.mode = 'over';
+        this.drawer.close();
+      } else {
+        this.drawer.mode = 'side';
+        this.drawer.close();
+      }
+    });
+  }
 }
