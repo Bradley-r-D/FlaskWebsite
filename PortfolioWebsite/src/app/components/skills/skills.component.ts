7812862d8a2fc566219875d359f020c0a616357f
Bradley Doodnath <bradleydood999@gmail.com>
2021-11-22 20:40:02 -0400

    added skills and formatted into a table

---
+++ PortfolioWebsite/src/app/components/skills/skills.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-skills',
+  templateUrl: './skills.component.html',
+  styleUrls: ['./skills.component.scss'],
+})
+export class SkillsComponent implements OnInit {
+  public mySkills: any[] = [
+    { skillName: 'Angular', skillLevel: 'Intermediate' },
+    { skillName: 'ReactNative', skillLevel: 'Intermediate' },
+    { skillName: 'JavaScript', skillLevel: 'Intermediate' },
+    { skillName: 'TypeScript', skillLevel: 'Intermediate' },
+    { skillName: 'HTML', skillLevel: 'Intermediate' },
+    { skillName: 'CSS', skillLevel: 'Intermediate' },
+    { skillName: 'Python', skillLevel: 'Basic' },
+    { skillName: 'Git', skillLevel: 'Intermediate' },
+    { skillName: 'GitHub', skillLevel: 'Intermediate' },
+    { skillName: 'SQL', skillLevel: 'Intermediate' },
+    { skillName: 'MySQL', skillLevel: 'Intermediate' },
+    { skillName: 'PHP', skillLevel: 'Intermediate' },
+    { skillName: 'ASM', skillLevel: 'Basic' },
+    { skillName: 'Microsoft Office', skillLevel: 'Advanced' },
+    { skillName: 'Microsoft Excel', skillLevel: 'Advanced' },
+    { skillName: 'Microsoft Word', skillLevel: 'Advanced' },
+    { skillName: 'Microsoft PowerPoint', skillLevel: 'Advanced' },
+    { skillName: 'Microsoft Power Automate', skillLevel: 'Advanced' },
+    { skillName: 'Microsoft Power Apps ', skillLevel: 'Intermediate' },
+    { skillName: 'Google DialogFlow ', skillLevel: 'Intermediate' },
+    { skillName: 'Electrical Circuit Analysis', skillLevel: 'Intermediate' },
+  ];
+  constructor() {}
+
+  ngOnInit(): void {}
+}
